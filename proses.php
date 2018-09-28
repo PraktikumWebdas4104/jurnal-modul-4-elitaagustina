ce5e238171f817d57d4053b236208c2b22e20c75
elitaagustina <43506640+elitaagustina@users.noreply.github.com>
2018-09-28 10:05:21 +0700

    Update proses.php
    NAMA = ELITA AGUSTINA SITANGGANG
    NIM = 6701174071
    KELAS =D3MI4104

--- proses.php
+++ proses.php
@@ -1,4 +1,24 @@
-<?php
+<form action="" method="post">
+<input type="checkbox" name="hobi[]" value="olahraga">OLAHRAGA<br/>
+<input type="checkbox" name="hobi[]" value="berenang">BERENANG<br/>
+<input type="checkbox" name="hobi[]" value="main sepeda">MAIN SEPEDA<br/>
+<input type="checkbox" name="hobi[]" value="main gitar">MAIN GITAR<br/>
+ <input type="reset" value="Delete Hobi" name="Delete"> <br> <br>
 
 
+    Select untuk upload image:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit"> <br><br>
+
+    <input type="submit" name="kirim" value="kirim">
+</form>
+
+<?php 
+if (isset($_POST['kirim'])){
+	$data=$_POST["hobi"];
+
+		foreach ($data as $nilai) {
+			echo $nilai."<br/>";
+		}	
+}
 ?>
